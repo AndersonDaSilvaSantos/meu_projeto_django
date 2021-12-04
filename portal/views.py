74238ca1f86cc9e7f2e7714836dfbb545c282522
Anderson <anderson_silva05@hotmail.com>
2021-12-04 15:56:20 -0300

    Criando formulários

--- portal/views.py
+++ portal/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 
+from portal.models import Autor
+
 
 def home(request):
     return render(request, 'portal/home.html')
+
+def dashboard(request):
+    return render(request, 'portal/dashbord.html')
+
+def autor(request):
+    autores = Autor.objects.all()
+
+    context = {
+        'autores': autores
+    }
+    return render(request, 'portal/autor.html', context)
+
+def editora(request):
+    return render(request, 'portal/editora.html')
+
+def formato(request):
+    return render(request, 'portal/formato.html')
+
+def livro(request):
+    return render(request, 'portal/livro.html')
