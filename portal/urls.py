ccd196c9703efe9a33d1074da77af8739b9193d0
Anderson <anderson_silva05@hotmail.com>
2021-12-05 09:08:58 -0300

    Inserindo dados no CRUD

--- portal/urls.py
+++ portal/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('autor/add/', views.autor_add, name='autor_add'),
     path('autor/', views.autor, name='autor'),
