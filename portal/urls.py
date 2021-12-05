9c837fbe848c54f1bd4b1ac74a5e442d21a1e9cb
Anderson <anderson_silva05@hotmail.com>
2021-12-05 09:27:06 -0300

    Editando dados no CRUD

--- portal/urls.py
+++ portal/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('autor/edit/int:autor_pk', views.autor_edit, name='autor_edit'),
     path('autor/add/', views.autor_add, name='autor_add'),
     path('autor/', views.autor, name='autor'),
     path('editora/', views.editora, name='editora'),
--- portal/views.py
+++ portal/views.py
@@ -32,6 +32,24 @@ def autor_add(request):
 
     return render(request,'portal/autor_add.html', context)
 
+def autor_edit(request, autor_pk):
+    autor = Autor.objects.get(pk=autor_pk)
+
+    form = AutorForm(request.POST or None, instance=autor)
+
+    if request.POST:
+        if form.is_valid():
+            form.save()
+            return redirect('autor')
+
+    context = {
+        'form': form
+    }
+
+    return render(request,'portal/autor_edit.html', context)
+
+
+
 def editora(request):
     return render(request, 'portal/editora.html')
 
